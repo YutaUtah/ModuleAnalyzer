3fc30ddfd36e726334cb41facfe09c98ac2b5e7e
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-18 00:01:09 +0900

    set up basicConfig with pretty format

--- pystats/__main__.py
+++ pystats/__main__.py
@@ -7,15 +7,27 @@
 #  inside `if __name__ == '__main__':`
 
 import sys
+import logging
 
-from .pystats import PyStatsApp
+# DEBUGGING needs to do "from pystats_app import PyStatsApp"
+# from command line, needs to do "from .pystats_app import PyStatsApp"
+from .pystats_app import PyStatsApp
+
+logging.basicConfig(
+    format='%(levelname)s: %(asctime)s [ %(filename)s line %(lineno)d] %(message)s', datefmt='%I:%M:%S %p %m/%d/%Y', level=logging.DEBUG, stream=sys.stdout
+)
+
+logger = logging.getLogger(__name__)
 
 args = PyStatsApp.parse_args(sys.argv[1:])
+logging.info(f'Parsing arguments from command line. INPUT : {args.input}, STATS: {args.stats}, REPORTS: {args.reports}, OUTPUT FILENAME: {args.output_filename}, VERBOSE: {args.verbose}')
 app = PyStatsApp(args.verbose)
 
-app.run(args.input,
-        filename_base=args.output,
-        stat_names=args.stats,
-        report_names=args.reports)
+app.run(
+    args.input,
+    filename_base=args.output_filename,
+    stat_names=args.stats,
+    report_names=args.reports
+)
 
-print('DONE.')
+logger.info(f'Successfully finished the job in {__name__}')
