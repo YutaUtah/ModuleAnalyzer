168e67ae54673a811399506106f46f313ac2e9c8
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-18 23:38:00 +0900

    nested in main()

--- pystats/__main__.py
+++ pystats/__main__.py
@@ -1,31 +1,35 @@
-# __main__.py
-#
-# This file is run when a user runs:
-# > python -m pystats
-#
-# Note this code was previously in pystats.py
-#  inside `if __name__ == '__main__':`
+'''
+This file is run when a user runs:
+> python -m pystats
+
+Note this code was previously in pystats.py
+ inside `if __name__ == '__main__':`
+'''
 
 import sys
 
 # DEBUGGING
-from pystats_app import PyStatsApp
-from utils.logging.logger import CustomLogger
+# from pystats_app import PyStatsApp
+# from utils.logging.logger import Logger
 
 # COMMANDLINE
-# from pystats.pystats_app import PyStatsApp
-# from pystats.utils.logging.logger import CustomLogger
+from pystats.pystats_app import PyStatsApp
+from pystats.utils.logging.logger import Logger
 
-logger = CustomLogger("INFO").get_custom_logger()
-args = PyStatsApp.parse_args(sys.argv[1:])
-logger.info(f'Parsing arguments from command line. INPUT : {args.input}, STATS: {args.stats}, REPORTS: {args.reports}, OUTPUT FILENAME: {args.output_filename}, VERBOSE: {args.verbose}')
-app = PyStatsApp(args.verbose)
+logger = Logger(__file__)
 
-app.run(
-    args.input,
-    filename_base=args.output_filename,
-    stat_names=args.stats,
-    report_names=args.reports
-)
+def main():
+    args = PyStatsApp.parse_args(sys.argv[1:])
+    logger.info(f'''Parsing arguments from command line. INPUT : {args.input}, STATS: {args.stats}, REPORTS: {args.reports}, OUTPUT FILENAME: {args.output_filename}, VERBOSE: {args.verbose}''')
+    app = PyStatsApp(args.verbose)
+    app.run(
+        args.input,
+        filename_base=args.output_filename,
+        stat_names=args.stats,
+        report_names=args.reports
+    )
+    logger.info(f'Successfully finished the job in {__name__}')
 
-logger.info(f'Successfully finished the job in {__name__}')
+
+if __name__ == "__main__":
+    main()
