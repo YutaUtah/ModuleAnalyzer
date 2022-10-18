820a87d51b980aa01a074c068eafd0cb5b4bf2e9
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-18 23:39:58 +0900

    Logger related

--- pystats/utils/logging/config.py
+++ pystats/utils/logging/config.py
@@ -1,27 +1,13 @@
-import logging
+from enum import Enum
 
-class Color:
-    GREY = "\x1b[38;20m"
+class Color(Enum):
     YELLOW = "\x1b[33;20m"
     RED = "\x1b[31;20m"
     BOLD_RED = "\x1b[31;1m"
     RESET = "\x1b[0m"
 
-class LoggerProperty:
-    FORMAT = "%(levelname)s:\t %(asctime)s %(message)s (%(filename)s:%(lineno)d)"
+class Format(Enum):
+    LOG_FORMAT = "%(levelname)s:\t %(asctime)s %(message)s (%(filename)s:%(lineno)d)"
 
-LEVEL = {
-    'DEBUG': logging.DEBUG,
-    'INFO': logging.INFO,
-    'WARNING': logging.WARNING,
-    'ERROR': logging.ERROR,
-    'CRITICAL': logging.CRITICAL,
-}
-
-FORMAT = {
-    logging.DEBUG: Color.RED + LoggerProperty.FORMAT + Color.RESET,
-    logging.INFO: Color.RED + LoggerProperty.FORMAT + Color.RESET,
-    logging.WARNING: Color.RED + LoggerProperty.FORMAT + Color.RESET,
-    logging.ERROR: Color.RED + LoggerProperty.FORMAT + Color.RESET,
-    logging.CRITICAL: Color.BOLD_RED + LoggerProperty.FORMAT + Color.RESET,
-}
+class DisplayFormat(Enum):
+    FORMAT = Color.RED.value + Format.LOG_FORMAT.value + Color.RESET.value
