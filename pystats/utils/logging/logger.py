bfe32e925b5bc03f15077e3fd45a89d260223f54
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-18 23:40:08 +0900

    created custom logger

--- pystats/utils/logging/logger.py
+++ pystats/utils/logging/logger.py
@@ -5,37 +5,73 @@ import sys
 # from pystats.utils.logging.config import LEVEL, FORMAT
 
 # DEBUG
-from .config import LEVEL, FORMAT
+from .config import DisplayFormat
 
+'''Outside of this module, you can use: logger = Logger(__file__) '''
+class Logger(logging.Formatter):
 
-class CustomFormatter(logging.Formatter):
-    datefmt='%I:%M:%S %p %m/%d/%Y'
-    FORMATS = FORMAT
-    stream=sys.stdout
+    LEVEL = {
+        'DEBUG': logging.DEBUG,
+        'INFO': logging.INFO,
+        'WARNING': logging.WARNING,
+        'ERROR': logging.ERROR,
+        'CRITICAL': logging.CRITICAL,
+    }
 
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-class CustomLogger:
-    def __init__(self, level):
-        self.logger = logging.getLogger(__name__)
-        self.custom_format = CustomFormatter()
-        self.logger_level = LEVEL.get(level.upper())
+    def __init__(self, filename, level="INFO"):
+        self.logger = logging.getLogger(filename)
+        self.logger_level = Logger.LEVEL.get(level.upper())
+        self._get_custom_logger()
 
     def _setLevel(self, logger_level):
         self.logger.setLevel(logger_level)
 
-    def _addStreamHandler(self):
-        ch = logging.StreamHandler()
-        ch.setFormatter(self.custom_format)
+    def _add_stream_Handler(self):
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setFormatter(logging.Formatter(DisplayFormat.FORMAT.value))
         self.logger.addHandler(ch)
-        self.logger.propagate = False
 
-    def get_custom_logger(self):
+    def _get_custom_logger(self):
         self._setLevel(self.logger_level)
         if not self.logger.handlers:
-            self._addStreamHandler()
+            self._add_stream_Handler()
         return self.logger
+
+    def getEffectiveLevel(self):
+        return self.logger.getEffectiveLevel()
+
+    def is_valid_logger(self, level, log):
+        if isinstance(self.getEffectiveLevel(), int):
+            return self.getEffectiveLevel() <= Logger.LEVEL.get(level) and len(log) > 0
+        return False
+
+    def debug(self, log):
+        if self.is_valid_logger("DEBUG", log):
+            self.logger.debug(log)
+        else:
+            self.logger.error("Text log is empty")
+
+    def info(self, log):
+        if self.is_valid_logger("INFO", log):
+                self.logger.info(log)
+        else:
+            self.logger.error("Text log is empty")
+
+    def warning(self, log):
+        if self.is_valid_logger("WARNING", log):
+            self.logger.warning(log)
+        else:
+            self.logger.error("Text log is empty")
+
+    def error(self, log):
+        if self.is_valid_logger("ERROR", log):
+            self.logger.error(log)
+        else:
+            self.logger.error("Text log is empty")
+
+    def critical(self, log):
+        if self.is_valid_logger("CRITICAL", log):
+            self.logger.critical(log)
+        else:
+            self.logger.error("Text log is empty")
 
