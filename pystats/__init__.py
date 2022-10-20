8273fcd1f0cdd022dab0c0e47bc23d9663cc2fab
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-20 23:03:15 +0900

    added argument pystats_app

--- pystats/__init__.py
+++ pystats/__init__.py
@@ -1,8 +1,6 @@
-# __init__.py
-# 
 # This file is run when `import pystats` (the package) is run.
 # The following imports make accessible under `pystats`:
-#    - each of our classes, and 
+#    - each of our classes, and
 #    - `report` and `statistic`
 #
 # For example, given this __init__.py file, a user could run:
@@ -10,7 +8,7 @@
 #  >> pystats.PyStatsApp           # Valid because `PyStatsApp` is imported below
 #  >> pystats.statistic.Statistic  # Valid because `statistic` is imported below
 
-from pystats.pystats_app import PyStatsApp
+from pystats.app.pystats_app import PyStatsApp
 from pystats.parsed_file import CodeBlock, ParsedFile
 
 from pystats.report import Report
