bae4c80886296cfc9bd6758512b14a3062ab8fa7
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-21 21:57:30 +0900

    replaced to PackageContext

--- pystats/__main__.py
+++ pystats/__main__.py
@@ -9,19 +9,20 @@ Note this code was previously in pystats.py
 import sys
 
 # DEBUGGING
-# from pystats_app import PyStatsApp
-# from utils.logging.logger import Logger
+from app.pystats_app import PyStatsApp, PackageContext
+from utils.logging.logger import Logger
 
 # COMMANDLINE
-from pystats.app.pystats_app      import PyStatsApp
-from pystats.utils.logging.logger import Logger
+# from pystats.app.pystats_app      import PyStatsApp
+# from pystats.utils.logging.logger import Logger
 
 logger = Logger(__file__)
 
 def main():
-    args = PyStatsApp.parse_args(sys.argv[1:])
+    args = PackageContext.parse_args(sys.argv[1:])
     logger.info(f'''Parsing arguments from command line. INPUT : {args.input}, STATS: {args.stats}, REPORTS: {args.reports}, OUTPUT FILENAME: {args.output_filename}, VERBOSE: {args.verbose}''')
     app = PyStatsApp(args.verbose)
+    # print(args.input)
     app.run(
         args.input,
         filename_base=args.output_filename,
