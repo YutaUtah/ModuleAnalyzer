a93cb3654b5c5215336a1e53fbe62782e48a46be
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-23 21:21:37 +0900

    added printTree

--- pystats/app/pystats_app.py
+++ pystats/app/pystats_app.py
@@ -26,6 +26,7 @@ except:
 
 
 logger = Logger(__name__).logger
+
 # With a criteria (skip hidden files)
 def is_not_hidden(path):
     return not path.name.startswith(".")
@@ -43,10 +44,64 @@ class PyStatsApp:
 
     def __repr__(self):
         return (
+            f"<class '{__name__}'>\n"
             f'PyStatsApp(num_modules={len(self.modules)},\n'
             f'           num_stats={len(self.stats)},\n'
             f'           verbose={self.verbose})\n'
         )
+
+
+    def printTree(self, python_packages):
+        '''
+        Print the tree structure of specified python_packages.
+
+        Argument value you pass must be a list (str type not accepted)
+
+        EX)
+        pystats/
+        ├── __init__.py
+        ├── __main__.py
+        └── app/
+            ├── __init__.py
+            └── pystats_app.py
+        '''
+
+        try:
+            for python_package in python_packages:
+                DisplayablePath.printTree(python_package)
+
+        except (FileNotFoundError, TypeError):
+            if not isinstance(python_packages, list):
+                raise TypeError(f'type mismatch (python_packages): list is expected for {python_packages}')
+            if PackageContext.is_package(python_packages):
+                raise FileNotFoundError(f'not package: {python_package}')
+
+
+    def getPaths(self, python_packages):
+        '''
+        Get the list of package paths that are displayed as absolute path
+
+        EX)
+        /Users/yutahayashi/VisualStudioProjects/ModuleAnalyzer/pystats/__init__.py
+        /Users/yutahayashi/VisualStudioProjects/ModuleAnalyzer/pystats/__main__.py
+        /Users/yutahayashi/VisualStudioProjects/ModuleAnalyzer/pystats/app/__init__.py
+        /Users/yutahayashi/VisualStudioProjects/ModuleAnalyzer/pystats/app/pystats_app.py
+        '''
+
+        file_hierarchy_list = []
+
+        try:
+            for python_package in python_packages:
+                file_hierarchy_list.append(DisplayablePath.get_sorted_paths(python_package))
+
+        except (FileNotFoundError, TypeError):
+            if not isinstance(python_packages, list):
+                raise TypeError(f'type mismatch (python_packages): list is expected for {python_packages}')
+            if PackageContext.is_package(python_packages):
+                raise FileNotFoundError(f'not package: {python_package}')
+
+
+        return file_hierarchy_list
 
 
     def write_report(
@@ -78,7 +133,6 @@ class PyStatsApp:
             if self.verbose:
                 logger.error(f'Error saving {report.name()} to "{out_filename}".')
 
-
     def run(
         self,
         python_filenames,
@@ -93,25 +147,28 @@ class PyStatsApp:
         '''
 
         # Map each
-        requested_stats = PackageContext.AVAILABLE_STATS
         if stat_names:
             requested_stats = [
                 s for s in PackageContext.AVAILABLE_STATS if s.name() in stat_names
             ]
+        else:
+            requested_stats = PackageContext.AVAILABLE_STATS
 
-        requested_reports = PackageContext.AVAILABLE_REPORTS
         if report_names:
             requested_reports = [
                 r for r in PackageContext.AVAILABLE_REPORTS
                 if r.name() in report_names
             ]
+        else:
+            requested_reports = PackageContext.AVAILABLE_REPORTS
 
         if package_stats_names:
             requested_package_name = [
                 s for s in PackageContext.PACKAGE_STATS if s.name() in package_stats_names
             ]
+        else:
+            package_stats_names = PackageContext.PACKAGE_STATS
 
-        # given_package = python_filenames[0]
 
         # scenario when its package
         if PackageContext.is_package(python_filenames):
@@ -131,7 +188,6 @@ class PyStatsApp:
                 # logger.info(f'Root Dir: {root}, Directory: {dirs}, File Names: {filenames}, Package Depth: {PackageContext.get_package_depth(root)}')
                 package_layer.append(package_layer_info)
 
-            DisplayablePath.printTree(python_filenames[0])
 
         # 1. PARSE MODULES
         self.modules = PackageContext.parse_modules(
