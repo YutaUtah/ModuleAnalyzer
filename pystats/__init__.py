9c0c698e34d5b4a094298f0bfadf321fcc540ee2
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-24 19:06:03 +0900

    built the logic for file generations

--- pystats/__init__.py
+++ pystats/__init__.py
@@ -11,8 +11,31 @@
 from __future__ import annotations
 
 # >> import pystats
-# ['Logger', 'PackageContext', 'PyStatsApp', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'app', 'config', 'context', 'parsed_file', 'report', 'statistic', 'utils']
 # >>> pystats.PackageContext
 # <class 'pystats.context.package_context.PackageContext'>
+try:
+    from pystats.app.pystats_app import PyStatsApp
+except:
+    from app.pystats_app         import PyStatsApp
 
-from pystats.app.pystats_app      import PyStatsApp
+
+paths = PyStatsApp(verbose=True).getPaths(['pystats'])
+
+import os
+for l in paths:
+    temp_dir_collection = set()
+    os.makedirs('./pystats_report/', exist_ok=True)
+    for path in l[1:]:
+        report_name = './' + str(path.relative_to(os.getcwd())).split('/')[0] + '_report'
+        sub_dir_path = '/'.join(str(path.relative_to(os.getcwd())).split('/')[1:-1])
+        sub_file_path = '/'.join(str(path.relative_to(os.getcwd())).split('/')[1:]) if len(str(path.relative_to(os.getcwd())).split('/')) != 2 else ''
+        report_path = os.path.join(os.getcwd(), report_name)
+        if sub_dir_path not in temp_dir_collection:
+            os.makedirs(os.path.join(report_name, sub_dir_path), exist_ok=True)
+            temp_dir_collection.add(sub_dir_path)
+        if len(sub_file_path) == 0:
+            sub_file_path = str(path).split('/')[-1]
+        target_md_path = os.path.join(report_name, sub_file_path).replace('.py', '.md')
+        with open(target_md_path, mode='w') as f:
+            f.write('hello!')
+# PyStatsApp(verbose=True).printTree(['pystats'])
