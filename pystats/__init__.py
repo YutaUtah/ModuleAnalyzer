f91a3ed7c31f204ef105fc252c2c77babfe2321f
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-10-26 12:01:50 +0900

    added run func

--- pystats/__init__.py
+++ pystats/__init__.py
@@ -1,12 +1,14 @@
-# This file is run when `import pystats` (the package) is run.
-# The following imports make accessible under `pystats`:
-#    - each of our classes, and
-#    - `report` and `statistic`
-#
-# For example, given this __init__.py file, a user could run:
-#  >> import pystats
-#  >> pystats.PyStatsApp           # Valid because `PyStatsApp` is imported below
-#  >> pystats.statistic.Statistic  # Valid because `statistic` is imported below
+'''
+This file is run when `import pystats` (the package) is run.
+The following imports make accessible under `pystats`:
+   - each of our classes, and
+   - `report` and `statistic`
+
+For example, given this __init__.py file, a user could run:
+ >> import pystats
+ >> pystats.PyStatsApp           # Valid because `PyStatsApp` is imported below
+ >> pystats.statistic.Statistic  # Valid because `statistic` is imported below
+'''
 
 from __future__ import annotations
 
@@ -20,5 +22,9 @@ except:
 
 
 # paths = PyStatsApp(verbose=True).getReports(packagename_path=['/Users/yutahayashi/VisualStudioProjects/ModuleAnalyzer/pystats', '/Users/yutahayashi/VisualStudioProjects/ModuleAnalyzer/pystats/utils'])
+paths = PyStatsApp(verbose=True).run(
+        ['/Users/yutahayashi/VisualStudioProjects/ModuleAnalyzer/pystats'],
+    )
+    # print(args): Namespace(input=['statistic.py'], stats=[], reports=[], output_filename=['out'], verbose=True)
 
-# PyStatsApp(verbose=True).getReports()
+# PyStatsApp(verbose=True).printTree(['pystats'])
