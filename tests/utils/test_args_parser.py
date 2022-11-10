8d7991bcede593c13234035d94b2f04cedbf643e
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-11-10 21:29:09 +0900

    added test for argparser

---
+++ tests/utils/test_args_parser.py
@@ -0,0 +1,12 @@
+import unittest
+
+from pystats.utils.args_parser import add_parser_options
+from pystats.context.package_context import PackageContext
+
+
+class TestGetCodeBlocks(unittest.TestCase):
+
+    def test_happy(self):
+        app = add_parser_options(PackageContext)
+        args = app.parse_args('-h')
+        self.assertEqual(args.input, ['-', 'h'])
